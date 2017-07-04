eadaf80c2ae95acbd54306ad3a889d4830fb8c1b
Jason Casares <jasonc@csdrafting.com>
2017-07-04 00:26:23 -0700

    add librarylist js

--- App.js
+++ App.js
@@ -4,12 +4,13 @@ import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import reducers from './src/reducers';
 import { Header } from './components/common';
-
+import LibraryList from './src/components/LibraryList';
 const App = () => {
   return (
     <Provider store={createStore(reducers)}>
       <View>
         <Header headerText="Text Stack" />
+        <LibraryList />
       </View>
     </Provider>
   );
---
+++ src/components/LibraryList.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+class Library List extends Component {
+  render() {
+    return;
+  }
+}
+
+const mapStateToProps = state => {
+  
+};
+
+export default connect(mapStateToProps)(LibraryList);
