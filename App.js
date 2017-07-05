09bdc84f1703cec2b66c38fdee86d5e38b4aeb34
Jason Casares <jasonc@csdrafting.com>
2017-07-04 23:23:53 -0700

    add listitem.js

--- App.js
+++ App.js
@@ -8,7 +8,7 @@ import LibraryList from './src/components/LibraryList';
 const App = () => {
   return (
     <Provider store={createStore(reducers)}>
-      <View>
+      <View style={{ flex: 1 }}>
         <Header headerText="Text Stack" />
         <LibraryList />
       </View>
--- src/components/LibraryList.js
+++ src/components/LibraryList.js
@@ -1,14 +1,17 @@
 import React, { Component } from 'react';
+import { ListView } from 'react-native';
 import { connect } from 'react-redux';
+
 
 class Library List extends Component {
   render() {
+
     return;
   }
 }
 
 const mapStateToProps = state => {
-  
+  return { libraries: state.libraries };
 };
 
 export default connect(mapStateToProps)(LibraryList);
